d6e4298112f291e28607580ab18806f4ac015dac
vitertik <cF!%eq34>
2021-09-06 14:23:28 -0400

    add memoization for sorting messages and minor changes(dele unnecessary dependences, hide password in server/db)

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
-  const sortedMessages = messages.sort((item1, item2) => new Date(item1.createdAt) - new Date(item2.createdAt))
-
+  const sortedMessages = useMemo(() => {
+    return messages.sort(
+      (item1, item2) => new Date(item1.createdAt) - new Date(item2.createdAt)
+    );
+  }, [messages]);
   return (
     <Box>
       {sortedMessages.map((message) => {
@@ -15,7 +18,12 @@ const Messages = (props) => {
         return message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} />
         ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+          <OtherUserBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+          />
         );
       })}
     </Box>
